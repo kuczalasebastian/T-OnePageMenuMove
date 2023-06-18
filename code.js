c9e3d8b04711017207b1de0ebb3d2e6d76268332
kuczala.sebastian <kuczalasebastian97@gmail.com>
2023-06-18 13:40:31 +0200

    T:MoveMenuOnePage

--- code.js
+++ code.js
@@ -1,14 +1,9 @@
-window.onscroll = function()
-{myFunction()};
-
-let navbar = document.getElementById("navbar");
-
-let sticky = navbar.offsetTop;
-
-function myFunction() {
-    if(window.pageYOffset >= sticky){
-        navbar.classList.add("sticky")
+window.addEventListener("scroll", function() {
+    var navbar = document.getElementById("navbar");
+    if (window.pageYoffset > 0) {
+      navbar.classList.add("sticky");
     } else {
-        navbar.classList.remove("sticky");
+      navbar.classList.remove("sticky");
     }
-}
+  });
+  
